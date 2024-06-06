62f6c8e5ae094d1636307ea9a1a9d0e60d2def56
NLazutkin <OKUMA02@yandex.ru>
2024-06-06 17:19:35 +0300

    rev. 1.5.2 - Прохождение автотестов checkstyle класс Managers пробелы между методами

--- src/manage/Managers.java
+++ src/manage/Managers.java
@@ -2,9 +2,11 @@ package manage;
 
 public final class Managers {
     private Managers() { }
+
     public static TaskManager getDefault() {
         return new InMemoryTaskManager();
     }
+
     public static HistoryManager getDefaultHistory() {
         return new InMemoryHistoryManager();
     }
